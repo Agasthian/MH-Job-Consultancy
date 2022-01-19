f918ccbb99540e099d0d132f32dfe08693f63ddb
Agasthian <agasthian_r@hotmail.com>
2022-01-19 21:11:13 +0530

    Added mobile navbar and responsive bug fix

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// Mobile Menu
+const hamburger = document.getElementById('hamburger');
+const navUL = document.getElementById('nav-ul');
+
+hamburger.addEventListener('click', ()=>{
+    navUL.classList.toggle('show');
+})
